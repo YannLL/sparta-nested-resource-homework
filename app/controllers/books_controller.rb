c8cdb48235c60fea085c55e4662f7259dce51745
Tech-A08 <tech-a08@MacBook-Air-3.local>
2018-06-24 16:43:26 +0100

    homework complete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,9 +57,10 @@ class BooksController < ApplicationController
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
+    @author = Author.find(params[:author_id])
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.html { redirect_to author_path(@author), notice: 'Book was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
